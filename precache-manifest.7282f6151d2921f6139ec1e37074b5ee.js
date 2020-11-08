32b96522a0b32e813dc0748dbffa175845afed23
Ren <nsoharab@gmail.com>
2020-11-09 00:02:14 +0200

    Updates

---
+++ precache-manifest.7282f6151d2921f6139ec1e37074b5ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "546155c491bad581fe76a56daad8128f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4680e18e42f740c7c13e",
+    "url": "./static/js/2.7554baf4.chunk.js"
+  },
+  {
+    "revision": "177f179dbee68262dea2e12f4e434276",
+    "url": "./static/js/2.7554baf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45bfeb160c33c6854cf8",
+    "url": "./static/js/main.71633a52.chunk.js"
+  },
+  {
+    "revision": "bb45fac8ed9d34ab38b4",
+    "url": "./static/js/runtime-main.49ef8e8e.js"
+  }
+]);
